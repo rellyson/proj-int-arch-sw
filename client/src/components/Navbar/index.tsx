ba25f8cf9df6ac5befe07cac292950fc87c68da4
Rellyson Silva <rellyson@proton.me>
2022-09-16 09:59:25 -0300

    feat: navbar branding

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -9,11 +9,14 @@ import {
   MenuItem,
   MenuDivider,
   useColorMode,
+  Image,
+  Text,
 } from '@chakra-ui/react'
 import { MdLogout } from 'react-icons/md'
 import React, { FC, useEffect } from 'react'
 import { logOut } from '../../services/auth'
 import { UserInfo } from '../../services/user'
+import logo from '../../assets/logo.png'
 
 const NavBar: FC = () => {
   let userInfo: UserInfo = {
@@ -35,7 +38,12 @@ const NavBar: FC = () => {
       direction="row"
       alignContent="space-between"
     >
-      <Box w="35%"></Box>
+      <Box w="35%" display="flex" flexDirection="row">
+        <Image ml="4" pt="1" src={logo} w={12}></Image>
+        <Text ml="2" fontWeight="semibold" pt="4">
+          Gerenciador de senhas
+        </Text>
+      </Box>
       <Box w="62%"></Box>
       <Box w="3%" display="flex" flexDirection="row">
         <Menu>
